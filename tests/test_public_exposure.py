c1ab55666e84b894e31443ce4e181676d73b6e53
Cesar Rodriguez <therasec@gmail.com>
2017-09-16 05:38:24 -0400

    fixes tests to check for public defaults

--- tests/test_public_exposure.py
+++ tests/test_public_exposure.py
@@ -15,6 +15,7 @@ class TestPublicExposure(unittest.TestCase):
 
     def test_aws_alb_public(self):
         self.v.enable_variable_expansion()
+        self.v.error_if_property_missing()
         self.v.resources(
             'aws_alb').property('internal').should_not_equal(False)
 
@@ -26,12 +27,14 @@ class TestPublicExposure(unittest.TestCase):
 
     def test_aws_dms_replication_instance_public(self):
         self.v.enable_variable_expansion()
+        self.v.error_if_property_missing()
         self.v.resources(
             'aws_dms_replication_instance').property(
             'publicly_accessible').should_not_equal(True)
 
     def test_aws_elb_public(self):
         self.v.enable_variable_expansion()
+        self.v.error_if_property_missing()
         self.v.resources(
             'aws_elb').property('internal').should_not_equal(False)
 
@@ -55,6 +58,7 @@ class TestPublicExposure(unittest.TestCase):
 
     def test_aws_redshift_cluster_public(self):
         self.v.enable_variable_expansion()
+        self.v.error_if_property_missing()
         self.v.resources(
             'aws_redshift_cluster').property(
             'publicly_accessible').should_not_equal(True)
