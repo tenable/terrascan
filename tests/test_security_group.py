f2feaa37707417abd5be534b063e12d52d234344
Cesar Rodriguez <therasec@gmail.com>
2017-09-15 22:55:22 -0400

    removes uneccessary class

--- tests/test_security_group.py
+++ tests/test_security_group.py
@@ -4,7 +4,7 @@ import terraform_validate
 from . import settings
 
 
-class TestSgsOutsideOfVpc(unittest.TestCase):
+class TestSecurityGroups(unittest.TestCase):
 
     def setUp(self):
         # Tell the module where to find your terraform configuration folder
@@ -14,32 +14,22 @@ class TestSgsOutsideOfVpc(unittest.TestCase):
         self.v = terraform_validate.Validator(self.path)
 
     def test_aws_db_security_group_used(self):
-        # Assert resource has a KMS with CMK
+        # This SG type exists outside of VPC (e.g. ec2 classic)
         self.assertEqual(self.v.resources(
             'aws_db_security_group').resource_list, [])
 
     def test_aws_redshift_security_group_used(self):
-        # Assert resource has a KMS with CMK
+        # This SG type exists outside of VPC (e.g. ec2 classic)
         self.assertEqual(self.v.resources(
             'aws_redshift_security_group').resource_list, [])
 
     def test_aws_elasticache_security_group_used(self):
-        # Assert resource has a KMS with CMK
+        # This SG type exists outside of VPC (e.g. ec2 classic)
         self.assertEqual(self.v.resources(
             'aws_elasticache_security_group').resource_list, [])
 
-
-class TestSgsWithOpenIngress(unittest.TestCase):
-
-    def setUp(self):
-        # Tell the module where to find your terraform configuration folder
-        self.path = os.path.join(
-            os.path.dirname(
-                os.path.realpath(__file__)), settings.TERRAFORM_LOCATION)
-        self.v = terraform_validate.Validator(self.path)
-
     def test_aws_security_group_rule_open(self):
-        # Assert that listener port is 443
+        # Assert that ingress rule is open to 0.0.0.0/0
         self.v.enable_variable_expansion()
         self.v.resources(
             'aws_security_group_rule').with_property(
@@ -47,7 +37,7 @@ class TestSgsWithOpenIngress(unittest.TestCase):
             'cidr_blocks').list_should_not_contain('0.0.0.0/0')
 
     def test_aws_security_group_inline_rule_open(self):
-        # Assert that listener port is 443
+        # Assert that SG has ingress rule open to 0.0.0.0/0
         self.v.enable_variable_expansion()
         self.v.resources(
             'aws_security_group').property(
