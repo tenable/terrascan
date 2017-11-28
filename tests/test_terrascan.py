961e8e75c98d509d4b8de4d4ea7c4b8782a8806c
Cesar Rodriguez <therasec@gmail.com>
2017-11-27 21:31:29 -0800

    adds simple test

--- tests/test_terrascan.py
+++ tests/test_terrascan.py
@@ -4,19 +4,26 @@
 """Tests for `terrascan` package."""
 
 
-import unittest
-
+from unittest import TestCase
 from terrascan import terrascan
 
 
-class TestTerrascan(unittest.TestCase):
-    """Tests for `terrascan` package."""
-
-    def setUp(self):
-        """Set up test fixtures, if any."""
-
-    def tearDown(self):
-        """Tear down test fixtures, if any."""
-
-    def test_000_something(self):
-        """Test something."""
+class CliTestCase(TestCase):
+    def test_with_empty_args(self):
+        """
+        User passes no args, should fail with SystemExit
+        """
+        with self.assertRaises(SystemExit):
+            terrascan.main(['-h'])
+'''
+    def test_success(self):
+        """
+        Test successful terraform templates
+        """
+        result = terrascan.main([
+            '-l',
+            'tests/infrastructure/success',
+            '-t',
+            'all'
+            ])
+'''
