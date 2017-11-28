b44e9873998bc54c149dffd0380875227ea5a686
Cesar Rodriguez <therasec@gmail.com>
2017-11-27 22:18:38 -0800

    adding simple tests for the cli

--- tests/test_terrascan.py
+++ tests/test_terrascan.py
@@ -9,21 +9,30 @@ from terrascan import terrascan
 
 
 class CliTestCase(TestCase):
+
     def test_with_empty_args(self):
         """
         User passes no args, should fail with SystemExit
         """
         with self.assertRaises(SystemExit):
             terrascan.main(['-h'])
-'''
+
     def test_success(self):
         """
         Test successful terraform templates
         """
-        result = terrascan.main([
-            '-l',
-            'tests/infrastructure/success',
-            '-t',
-            'all'
+        with self.assertRaises(SystemExit):
+            terrascan.main([
+                '-l',
+                'tests/infrastructure/success',
             ])
-'''
+
+    def test_fail(self):
+        """
+        Test successful terraform templates
+        """
+        with self.assertRaises(SystemExit):
+            terrascan.main([
+                '-l',
+                'tests/infrastructure/fail',
+            ])
