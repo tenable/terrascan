99c39510ff917ee716112cca6e7c28e78d5dd1ce
Cesar Rodriguez <therasec@gmail.com>
2020-01-01 21:22:30 -0500

    adds short version of the readme to prevent rendering issues in pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open('README_short.rst') as readme_file:
     readme = readme_file.read()
 
 with open('HISTORY.rst') as history_file:
