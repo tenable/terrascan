61c3224dc05743196632b59b5959dad5c779655e
Yusuf Kanchwala <yusuf@accurics.com>
2020-07-23 23:57:50 +0530

    add gofmt, govet and golint validation scripts

---
+++ scripts/validate-gofmt.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+find_files() {
+  find . -not \( \
+      \( \
+        -wholename '*/vendor/*' \
+      \) -prune \
+    \) -name '*.go'
+}
+
+bad_files=$(find_files | xargs gofmt -d -s 2>&1)
+if [[ -n "${bad_files}" ]]; then
+  echo "${bad_files}" >&2
+  echo >&2
+  echo "fix gofmt errors by running:\n./hack/fix-gofmt.sh" >&2
+  exit 1
+fi
