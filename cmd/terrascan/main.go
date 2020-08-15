b757c7189564f93c46ea7d3f8208c551af9823ea
Willie Sana <willie@accurics.com>
2020-08-14 21:57:55 -0700

    add cobra command support

--- cmd/terrascan/main.go
+++ cmd/terrascan/main.go
@@ -16,76 +16,8 @@
 
 package main
 
-import (
-	"flag"
-	"fmt"
-
-	"github.com/accurics/terrascan/pkg/cli"
-	httpServer "github.com/accurics/terrascan/pkg/http-server"
-	"github.com/accurics/terrascan/pkg/initialize"
-	"github.com/accurics/terrascan/pkg/logging"
-	"github.com/accurics/terrascan/pkg/version"
-	"go.uber.org/zap"
-)
+import "github.com/accurics/terrascan/pkg/cli"
 
 func main() {
-
-	// command line flags
-	var (
-		// server mode
-		server = flag.Bool("server", false, "run terrascan in server mode")
-
-		// IaC flags
-		iacType     = flag.String("iac", "", "IaC provider (supported values: terraform)")
-		iacVersion  = flag.String("iac-version", "v12", "IaC version (supported values: 'v12' for terraform)")
-		iacFilePath = flag.String("f", "", "IaC file path")
-		iacDirPath  = flag.String("d", ".", "IaC directory path")
-		policyPath  = flag.String("p", "", "Policy directory path")
-
-		// cloud flags
-		cloudType = flag.String("cloud", "", "cloud provider (supported values: aws, azure)")
-
-		// logging flags
-		logLevel = flag.String("log-level", "info", "logging level (debug, info, warn, error, panic, fatal)")
-		logType  = flag.String("log-type", "console", "log type (json, console)")
-
-		// config file
-		configFile = flag.String("config", "", "config file path")
-
-		// output type
-		output = flag.String("output", "yaml", "output format (json, yaml)")
-
-		//version
-		ver = flag.Bool("version", false, "terrascan version")
-	)
-	flag.Parse()
-
-	// if no flags are passed, print usage
-	if flag.NFlag() < 1 {
-		flag.Usage()
-		return
-	}
-
-	// print version
-	if *ver {
-		fmt.Println(version.Get())
-		return
-	}
-
-	// initialize logger
-	logging.Init(*logType, *logLevel)
-
-	// initialize terrascan
-	if err := initialize.Run(); err != nil {
-		zap.S().Error("failed to initialize terrascan")
-		return
-	}
-
-	// if server mode set, run terrascan as a server, else run it as CLI
-	if *server {
-		httpServer.Start()
-	} else {
-		zap.S().Debug("running terrascan in cli mode")
-		cli.Run(*iacType, *iacVersion, *cloudType, *iacFilePath, *iacDirPath, *configFile, *policyPath, *output)
-	}
+	cli.Execute()
 }
--- go.mod
+++ go.mod
@@ -8,13 +8,19 @@ require (
 	github.com/hashicorp/go-version v1.2.0
 	github.com/hashicorp/hcl/v2 v2.3.0
 	github.com/hashicorp/terraform v0.12.28
+	github.com/mitchellh/go-homedir v1.1.0
 	github.com/open-policy-agent/opa v0.22.0
 	github.com/pelletier/go-toml v1.8.0
 	github.com/pkg/errors v0.9.1
-	github.com/spf13/afero v1.3.2
+	github.com/spf13/afero v1.3.4
+	github.com/spf13/cobra v1.0.0
+	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/zclconf/go-cty v1.2.1
-	go.uber.org/zap v1.9.1
+	go.uber.org/zap v1.10.0
 	golang.org/x/net v0.0.0-20200625001655-4c5254603344 // indirect
+	golang.org/x/sys v0.0.0-20200814200057-3d37ad5750ed // indirect
 	gopkg.in/src-d/go-git.v4 v4.13.1
 	gopkg.in/yaml.v2 v2.3.0
+	k8s.io/apimachinery v0.18.8 // indirect
+	k8s.io/client-go v11.0.0+incompatible
 )
