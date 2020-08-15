7001128beaf76494e04f9a9256c1344fcea754e6
Cesar Rodriguez <cesar@accurics.com>
2020-08-15 16:05:37 -0400

    adds objects

--- docs/architecture.diagrams.py
+++ docs/architecture.diagrams.py
@@ -1,7 +1,37 @@
 from diagrams import Cluster, Diagram
-from diagrams.onprem.iac import Terraform
+from diagrams.onprem import iac
 from diagrams.onprem.client import Client
+from diagrams.onprem.compute import Server
+from diagrams.aws.compute import ECS
+from diagrams.azure.compute import VM
+from diagrams.gcp.compute import GCE
 
 with Diagram("Terrascan architecture", show=False):
-    Client("CLI") >> Terraform("IaC Provider")
+    cli = Client("CLI")
+    server = Server("API server")
+
+    with Cluster("Runtime"):
+        runtime = [
+            ECS("Input Validate"),
+            ECS("Process"),
+            ECS("Output")
+        ]
+
+    with Cluster("IaC Providers"):
+        iac = [
+            iac.Terraform("Terraform"),
+            iac.Ansible("Ansible"),
+            iac.Awx("Awx")
+        ]
+
+
+    with Cluster("Policy Engine"):
+        runtime = [
+            ECS("AWS"),
+            VM("Azure"),
+            GCE("GCP")
+        ]
+
+    cli >> runtime >> iac >> policy
+    server >> runtime >> iac >> policy
 
