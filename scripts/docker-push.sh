c515621a80d2a3701906270816c9b8023401b0e9
Yusuf Kanchwala <yusuf@accurics.com>
2020-08-15 11:27:43 +0530

    update docker push to push docker images with latest tag

--- scripts/docker-push.sh
+++ scripts/docker-push.sh
@@ -7,6 +7,9 @@ set -o pipefail
 GIT_COMMIT=$(git rev-parse --short HEAD 2>/dev/null)
 DOCKER_REPO="accurics/terrascan"
 DOCKERFILE="./build/Dockerfile"
+LATEST_TAG="latest"
 
 # PS: It is a prerequisite to execute 'docker login' before running this script
+docker tag ${DOCKER_REPO}:${GIT_COMMIT} ${DOCKER_REPO}:${LATEST_TAG}
 docker push ${DOCKER_REPO}:${GIT_COMMIT}
+docker push ${DOCKER_REPO}:${LATEST_TAG}
