a30f5a94ebb1162a221c8901a2764c0effb09148
Willie Sana <willie@accurics.com>
2020-08-14 21:57:55 -0700

    add cobra command support

---
+++ pkg/cli/init.go
@@ -0,0 +1,36 @@
+/*
+    Copyright (C) 2020 Accurics, Inc.
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+package cli
+
+import (
+	"github.com/accurics/terrascan/pkg/initialize"
+	"github.com/accurics/terrascan/pkg/logging"
+	"github.com/spf13/cobra"
+	"go.uber.org/zap"
+)
+
+func initial(cmd *cobra.Command, args []string) {
+
+	// initialize logger
+	logging.Init(LogType, LogLevel)
+
+	// initialize terrascan
+	if err := initialize.Run(); err != nil {
+		zap.S().Error("failed to initialize terrascan")
+		return
+	}
+}
