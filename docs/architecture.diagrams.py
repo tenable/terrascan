b49af79075905d3f74e1fc635dc629267820ee5b
Cesar Rodriguez <cesar@accurics.com>
2020-08-15 21:39:58 -0400

    organizes full diagram

--- docs/architecture.diagrams.py
+++ docs/architecture.diagrams.py
@@ -1,26 +1,29 @@
 from diagrams import Cluster, Diagram
-from diagrams.onprem import iac
-from diagrams.onprem.client import Client
-from diagrams.onprem.compute import Server
 from diagrams.aws.compute import ECS
+from diagrams.aws.management import Cloudformation
+from diagrams.aws.integration import ConsoleMobileApplication
 from diagrams.azure.compute import VM
 from diagrams.gcp.compute import GCE
+from diagrams.programming.language import Bash
+from diagrams.onprem import iac
+from diagrams.onprem.compute import Server
+
 
 with Diagram("Terrascan architecture", show=False):
-    cli = Client("CLI")
+    cli = Bash("CLI")
     server = Server("API server")
+    notifier = ConsoleMobileApplication("Notifier (Webhook)")
+    writer = Bash("Writer (JSON, YAML, XML)")
 
     with Cluster("Runtime"):
-        runtime = [
-            ECS("Input Validate"),
-            ECS("Process"),
-            ECS("Output")
-        ]
+        ECS("Input Validate")
+        ECS("Process")
+        output = ECS("Output")
 
     with Cluster("IaC Providers"):
         tf = iac.Terraform("Terraform")
         ansible = iac.Ansible("Ansible")
-        iac.Awx("Awx")
+        cft = Cloudformation("CloudFormation")
 
 
     with Cluster("Policy Engine"):
@@ -30,6 +33,7 @@ with Diagram("Terrascan architecture", show=False):
             GCE("GCP")
         ]
 
-    server >> runtime >> tf >> policy
-    cli >> runtime >> ansible >> policy
+    server >> output >> tf >> policy >> notifier
+    cli >> output >> ansible >> policy >> writer
+    cft >> policy
 
