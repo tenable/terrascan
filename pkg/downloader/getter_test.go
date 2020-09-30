e72bf36820edf1310572aa89032077893b24b613
Yusuf Kanchwala <yusuf@accurics.com>
2020-09-30 19:33:50 +0530

    add unit test for downloader methods

--- pkg/downloader/getter_test.go
+++ pkg/downloader/getter_test.go
@@ -17,6 +17,7 @@
 package downloader
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 )
@@ -129,5 +130,117 @@ func TestGetURLSubDir(t *testing.T) {
 		if !reflect.DeepEqual(gotErr, tt.wantErr) {
 			t.Errorf("url got: '%v', want: '%v'", gotErr, tt.wantErr)
 		}
+	}
+}
+
+func TestDownload(t *testing.T) {
+
+	table := []struct {
+		name     string
+		URL      string
+		dest     string
+		wantDest string
+		wantErr  error
+	}{
+		{
+			name:     "empty URL",
+			URL:      "",
+			dest:     "some-destination",
+			wantDest: "",
+			wantErr:  ErrEmptyURLDest,
+		},
+		{
+			name:     "empty destination",
+			URL:      "some-url",
+			dest:     "",
+			wantDest: "",
+			wantErr:  ErrEmptyURLDest,
+		},
+		{
+			name:     "invalid url",
+			URL:      "github.com/some-repo",
+			dest:     "some-dest",
+			wantDest: "",
+			wantErr:  fmt.Errorf("GitHub URLs should be github.com/username/repo"),
+		},
+	}
+
+	for _, tt := range table {
+		t.Run(tt.name, func(t *testing.T) {
+			g := NewGoGetter()
+			gotDest, gotErr := g.Download(tt.URL, tt.dest)
+			if !reflect.DeepEqual(gotErr, tt.wantErr) {
+				t.Errorf("error got: '%v', want: '%v'", gotErr, tt.wantErr)
+			}
+			if !reflect.DeepEqual(gotDest, tt.wantDest) {
+				t.Errorf("dest got: '%v', want: '%v'", gotDest, tt.wantDest)
+			}
+		})
+	}
+}
+
+func TestDownloadWithType(t *testing.T) {
+
+	table := []struct {
+		name     string
+		Type     string
+		URL      string
+		dest     string
+		wantDest string
+		wantErr  error
+	}{
+		{
+			name:     "empty URL and Type",
+			Type:     "",
+			URL:      "",
+			dest:     "some-destination",
+			wantDest: "",
+			wantErr:  ErrEmptyURLType,
+		},
+		{
+			name:     "empty URL",
+			Type:     "some-type",
+			URL:      "",
+			dest:     "some-destination",
+			wantDest: "",
+			wantErr:  ErrEmptyURLDest,
+		},
+		{
+			name:     "empty Type",
+			Type:     "",
+			URL:      "some-url",
+			dest:     "some-destination",
+			wantDest: "",
+			wantErr:  ErrEmptyURLDest,
+		},
+		{
+			name:     "empty dest",
+			Type:     "some-type",
+			URL:      "some-url",
+			dest:     "",
+			wantDest: "",
+			wantErr:  ErrEmptyURLDest,
+		},
+		{
+			name:     "invalid url",
+			Type:     "some-type",
+			URL:      "github.com/some-url",
+			dest:     "some-dest",
+			wantDest: "",
+			wantErr:  fmt.Errorf("download not supported for scheme 'some-type'"),
+		},
+	}
+
+	for _, tt := range table {
+		t.Run(tt.name, func(t *testing.T) {
+			g := NewGoGetter()
+			gotDest, gotErr := g.DownloadWithType(tt.Type, tt.URL, tt.dest)
+			if !reflect.DeepEqual(gotErr, tt.wantErr) {
+				t.Errorf("error got: '%v', want: '%v'", gotErr, tt.wantErr)
+			}
+			if !reflect.DeepEqual(gotDest, tt.wantDest) {
+				t.Errorf("dest got: '%v', want: '%v'", gotDest, tt.wantDest)
+			}
+		})
 	}
 }
