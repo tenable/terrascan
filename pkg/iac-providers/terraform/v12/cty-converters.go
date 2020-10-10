11be46bbff0bd7cb590fb6e4bb06c22de40aa0dd
Yusuf Kanchwala <yusuf@accurics.com>
2020-10-11 01:33:28 +0530

    add cty map to golang map converter

--- pkg/iac-providers/terraform/v12/cty-converters.go
+++ pkg/iac-providers/terraform/v12/cty-converters.go
@@ -17,12 +17,17 @@
 package tfv12
 
 import (
+	"fmt"
+	"github.com/pkg/errors"
 	"github.com/zclconf/go-cty/cty"
 	"github.com/zclconf/go-cty/cty/gocty"
 )
 
 // list of available cty to golang type converters
-var ctyConverterFuncs = []func(cty.Value) (interface{}, error){ctyToStr, ctyToInt, ctyToBool, ctyToSlice}
+var (
+	ctyConverterFuncs       = []func(cty.Value) (interface{}, error){ctyToStr, ctyToInt, ctyToBool, ctyToSlice, ctyToMap}
+	ctyNativeConverterFuncs = []func(cty.Value) (interface{}, error){ctyToStr, ctyToInt, ctyToBool, ctyToSlice}
+)
 
 // ctyToStr tries to convert the given cty.Value into golang string type
 func ctyToStr(ctyVal cty.Value) (interface{}, error) {
@@ -51,4 +56,41 @@ func ctyToSlice(ctyVal cty.Value) (interface{}, error) {
 	var val []interface{}
 	err := gocty.FromCtyValue(ctyVal, &val)
 	return val, err
+}
+
+// ctyToMap tries to converts the incoming cty.Value into map[string]cty.Value
+// then for every key value of this map, tries to convert the cty.Value into
+// native golang value and create a new map[string]interface{}
+func ctyToMap(ctyVal cty.Value) (interface{}, error) {
+
+	var (
+		ctyValMap = ctyVal.AsValueMap() // map[string]cty.Value
+		val       = make(map[string]interface{})
+		allErrs   error
+	)
+
+	// cannot process an empty ctValMap
+	if len(ctyValMap) < 1 {
+		return nil, fmt.Errorf("empty ctyValMap")
+	}
+
+	// iterate over every key cty.Value pair, try to convert cty.Value into
+	// golang value
+	for k, v := range ctyValMap {
+		// convert cty.Value to native golang type based on cty.Type
+		for _, converter := range ctyNativeConverterFuncs {
+			resolved, err := converter(v)
+			if err == nil {
+				val[k] = resolved
+				break
+			}
+			allErrs = errors.Wrap(allErrs, err.Error())
+		}
+	}
+	if allErrs != nil {
+		return nil, allErrs
+	}
+
+	// hopefully successful!
+	return val, nil
 }
