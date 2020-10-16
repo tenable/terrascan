43c4970d12a755a82ac7e427c244e889c9f90b0e
Jon Jarboe <jon@accurics.com>
2020-10-16 00:31:25 -0700

    fix typo

--- scripts/validate-gofmt.sh
+++ scripts/validate-gofmt.sh
@@ -16,6 +16,7 @@ bad_files=$(find_files | xargs gofmt -d -s 2>&1)
 if [[ -n "${bad_files}" ]]; then
   echo "${bad_files}" >&2
   echo >&2
-  echo "fix gofmt errors by running:\n./hack/fix-gofmt.sh" >&2
+  echo "fix gofmt errors by running:" >&2
+  echo "./scripts/fix-gofmt.sh" >&2
   exit 1
 fi
