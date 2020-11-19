8a70f9ec41f4f9dd0752076a6bd9affacd511db9
Paul Bergeron <paul.bergeron@ultratendency.com>
2020-11-19 14:17:33 +0100

    Fix infinite loop on variable resolution

--- pkg/iac-providers/terraform/v12/variable-references.go
+++ pkg/iac-providers/terraform/v12/variable-references.go
@@ -148,6 +148,10 @@ func (r *RefResolver) ResolveVarRefFromParentModuleCall(varRef string) interface
 	if reflect.TypeOf(val).Kind() == reflect.String {
 		valStr := val.(string)
 		resolvedVal := strings.Replace(varRef, varExpr, valStr, 1)
+		if varRef == resolvedVal {
+			zap.S().Debugf("resolved str variable ref refers to self: '%v'", varRef)
+			return val
+		}
 		zap.S().Debugf("resolved str variable ref: '%v', value: '%v'", varRef, string(resolvedVal))
 		return r.ResolveStrRef(resolvedVal)
 	}
