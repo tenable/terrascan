df1bd8a77c0a1200cc72b0a037b1421c6d71fe0c
Devang Gaur <devang.gaur@accurics.com>
2021-02-03 16:17:59 +0530

    terrascan init will download new policies

--- pkg/cli/run_test.go
+++ pkg/cli/run_test.go
@@ -38,7 +38,7 @@ func TestMain(m *testing.M) {
 func setup() {
 	// to download the policies for Run test
 	// downloads the policies at $HOME/.terrascan
-	initial(nil, nil)
+	initialScan(nil, nil)
 }
 
 func shutdown() {
