0ad71c1705d297838ca15e11aed4378963d7c46d
Gaurav Gogia <16029099+gaurav-gogia@users.noreply.github.com>
2021-02-20 01:11:24 +0530

    fixed slice comparision util function

--- pkg/utils/slice.go
+++ pkg/utils/slice.go
@@ -1,10 +1,16 @@
 package utils
 
+import "sort"
+
 // IsSliceEqual checks if two slices of string are equal or not
 func IsSliceEqual(list1, list2 []string) bool {
 	if len(list1) != len(list2) {
 		return false
 	}
+
+	sort.Strings(list1)
+	sort.Strings(list2)
+
 	for i := range list1 {
 		if list1[i] != list2[i] {
 			return false
