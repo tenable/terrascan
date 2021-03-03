2296d3a8426941507a31b30cf8b2882d4636e8d0
Yusuf Kanchwala <30405568+kanchwala-yusuf@users.noreply.github.com>
2021-03-03 19:52:40 +0530

    Adds support for scanning tfplan json file (#562)
    * add support for scanning tfplan file
    * vendoring update for tfplan scanning
    * moving JQFilterWithQuery method to utils
    * add unit tests for jqhelper
    * add unit tests for tfplan iac provider
    * add method to validate tfplan json file, add more unit tests
    * fixing e2e tests for help

---
+++ pkg/iac-providers/tfplan/v1/load-file_test.go
@@ -0,0 +1,173 @@
+/*
+    Copyright (C) 2020 Accurics, Inc.
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+package tfplan
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"reflect"
+	"testing"
+
+	"github.com/accurics/terrascan/pkg/iac-providers/output"
+)
+
+func TestLoadIacFile(t *testing.T) {
+
+	table := []struct {
+		name     string
+		filePath string
+		tfplan   TFPlan
+		want     output.AllResourceConfigs
+		wantErr  error
+	}{
+		{
+			name:     "invalid filepath",
+			filePath: "not-there",
+			tfplan:   TFPlan{},
+			wantErr:  fmt.Errorf("failed to read tfplan JSON file. error: 'open not-there: no such file or directory'"),
+		},
+		{
+			name:     "invalid json",
+			filePath: "./testdata/invalid-json.json",
+			tfplan:   TFPlan{},
+			wantErr:  fmt.Errorf("invalid terraform json file; error: 'failed to decode tfplan json. error: 'invalid character 'I' looking for beginning of value''"),
+		},
+		{
+			name:     "invalid tfplan json",
+			filePath: "./testdata/invalid-tfplan.json",
+			tfplan:   TFPlan{},
+			wantErr:  fmt.Errorf("invalid terraform json file; error: 'terraform format version shoule be '0.1''"),
+		},
+		{
+			name:     "valid tfplan json",
+			filePath: "./testdata/valid-tfplan.json",
+			tfplan:   TFPlan{},
+			wantErr:  nil,
+		},
+	}
+
+	for _, tt := range table {
+		t.Run(tt.name, func(t *testing.T) {
+			_, gotErr := tt.tfplan.LoadIacFile(tt.filePath)
+			if !reflect.DeepEqual(gotErr, tt.wantErr) {
+				t.Errorf("unexpected error; gotErr: '%v', wantErr: '%v'", gotErr, tt.wantErr)
+			}
+		})
+	}
+
+	t.Run("validate tfplan iac output", func(t *testing.T) {
+		var (
+			tfplan             = TFPlan{}
+			tfplanFile         = "./testdata/valid-tfplan.json"
+			tfplanOutput       = "./testdata/valid-tfplan-resource-config.json"
+			wantErr      error = nil
+		)
+
+		got, err := tfplan.LoadIacFile(tfplanFile)
+		if !reflect.DeepEqual(err, wantErr) {
+			t.Errorf("error want: '%v', got: '%v'", wantErr, err)
+		}
+
+		gotBytes, _ := json.MarshalIndent(got, "", "  ")
+		gotBytes = append(gotBytes, []byte{'\n'}...)
+		wantBytes, _ := ioutil.ReadFile(tfplanOutput)
+		if !reflect.DeepEqual(bytes.TrimSpace(gotBytes), bytes.TrimSpace(wantBytes)) {
+			t.Errorf("unexpected error; got '%v', want: '%v'", string(gotBytes), string(wantBytes))
+		}
+	})
+}
+
+func TestIsValidTFPlanJSON(t *testing.T) {
+
+	tfplan := TFPlan{}
+
+	table := []struct {
+		name    string
+		tfjson  []byte
+		wantErr error
+	}{
+		{
+			name:    "invalid json",
+			tfjson:  []byte("I am invalid"),
+			wantErr: fmt.Errorf("failed to decode tfplan json. error: 'invalid character 'I' looking for beginning of value'"),
+		},
+		{
+			name:    "incorrect terraform format version",
+			tfjson:  []byte(`{"format_version": "bad version"}`),
+			wantErr: errIncorrectFormatVersion,
+		},
+		{
+			name:    "empty terraform version",
+			tfjson:  []byte(`{"format_version": "0.1"}`),
+			wantErr: errEmptyTerraformVersion,
+		},
+		{
+			name:    "valid tfplan json",
+			tfjson:  []byte(`{"format_version": "0.1", "terraform_version": "0.12.3"}`),
+			wantErr: nil,
+		},
+	}
+
+	for _, tt := range table {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tfplan.isValidTFPlanJSON(tt.tfjson)
+			if !reflect.DeepEqual(err, tt.wantErr) {
+				t.Errorf("error got: '%v', want: '%v'", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestGetTFID(t *testing.T) {
+
+	table := []struct {
+		name  string
+		input string
+		want  string
+	}{
+		{
+			name:  "empty input",
+			input: "",
+			want:  "",
+		},
+		{
+			name:  "regular terraform id",
+			input: "x.y",
+			want:  "x.y",
+		},
+		{
+			name:  "long terraform id",
+			input: "x.y.z",
+			want:  "y.z",
+		},
+		{
+			name:  "extra long terraform id",
+			input: "w.x.y.z",
+			want:  "y.z",
+		},
+	}
+
+	for _, tt := range table {
+		got := getTFID(tt.input)
+		if got != tt.want {
+			t.Errorf("got: '%v', want: '%v'", got, tt.want)
+		}
+	}
+
+}
---
+++ pkg/utils/jqhelper_test.go
@@ -0,0 +1,77 @@
+/*
+    Copyright (C) 2020 Accurics, Inc.
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+package utils
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestJQFilterWithQuery(t *testing.T) {
+
+	table := []struct {
+		name    string
+		jqQuery string
+		input   []byte
+		want    []byte
+		wantErr error
+	}{
+		{
+			name:    "invalid json",
+			jqQuery: "",
+			input:   []byte{},
+			want:    []byte{},
+			wantErr: fmt.Errorf("failed to decode input JSON. error: 'unexpected end of JSON input'"),
+		},
+		{
+			name:    "invalid query",
+			jqQuery: "am invalid",
+			input:   []byte("{}"),
+			want:    []byte(""),
+			wantErr: fmt.Errorf("failed to parse jq query. error: 'unexpected token \"invalid\"'"),
+		},
+		{
+			name:    "jq error",
+			jqQuery: "def f: f; f, f",
+			input:   []byte("{}"),
+			want:    []byte(""),
+			wantErr: nil,
+		},
+		{
+			name:    "simple query",
+			jqQuery: ".foo",
+			input:   []byte("{\"foo\": 128}"),
+			want:    []byte("128"),
+			wantErr: nil,
+		},
+	}
+
+	for _, tt := range table {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := JQFilterWithQuery(tt.jqQuery, tt.input)
+
+			if string(got) != string(tt.want) {
+				t.Errorf("got: '%v', want: '%v'", string(got), string(tt.want))
+			}
+
+			if !reflect.DeepEqual(err, tt.wantErr) {
+				t.Errorf("error got: '%v', want: '%v'", err, tt.wantErr)
+			}
+		})
+	}
+}
