d242c2ee7113f114d770eec580b5424c0bb75705
Devang Gaur <devang.gaur@accurics.com>
2021-05-05 19:52:57 +0530

    Gh action terrscan_atlantis release (#733)
    * Add terrascan_atlantis container build and push in release workflow
    
    * add a sample config.toml in atlantis/

---
+++ scripts/atlantis/docker-push-latest.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+GIT_COMMIT=$(git rev-parse --short HEAD 2>/dev/null)
+DOCKER_REPO="accurics/terrascan_atlantis"
+LATEST_TAG="latest"
+
+# PS: It is a prerequisite to execute 'docker login' before running this script
+docker tag ${DOCKER_REPO}:${GIT_COMMIT} ${DOCKER_REPO}:${LATEST_TAG}
+docker push ${DOCKER_REPO}:${LATEST_TAG}
