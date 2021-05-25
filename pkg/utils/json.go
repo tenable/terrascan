e54cac86f1f0bd71d774a19a6c6e435f200851fb
Devang <devang.gaur.7@gmail.com>
2021-05-25 22:57:15 +0530

    added unit test

--- pkg/utils/json.go
+++ pkg/utils/json.go
@@ -2,8 +2,11 @@ package utils
 
 import (
 	"bufio"
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"os"
+	"reflect"
 )
 
 const (
@@ -49,4 +52,24 @@ func LoadJSON(filePath string) ([]*IacDocument, error) {
 	})
 
 	return iacDocumentList, nil
+}
+
+// AreEqualJSON validate if two json strings are equal
+func AreEqualJSON(s1, s2 string) (bool, error) {
+	var o1 interface{}
+	var o2 interface{}
+
+	errmsg := "error json unmarshalling string: %s. error: %v"
+
+	var err error
+	err = json.Unmarshal([]byte(s1), &o1)
+	if err != nil {
+		return false, fmt.Errorf(errmsg, s1, err.Error())
+	}
+	err = json.Unmarshal([]byte(s2), &o2)
+	if err != nil {
+		return false, fmt.Errorf(errmsg, s2, err.Error())
+	}
+
+	return reflect.DeepEqual(o1, o2), nil
 }
---
+++ pkg/writer/sarif_test.go
@@ -0,0 +1,150 @@
+package writer
+
+import (
+	"bytes"
+	"github.com/accurics/terrascan/pkg/policy"
+	"github.com/accurics/terrascan/pkg/results"
+	"github.com/accurics/terrascan/pkg/utils"
+	"strings"
+	"testing"
+)
+
+const expectedSarifOutput1 = `{
+          "version": "2.1.0",
+          "$schema": "https://raw.githubusercontent.com/oasis-tcs/sarif-spec/master/Schemata/sarif-schema-2.1.0.json",
+          "runs": [
+            {
+              "tool": {
+                "driver": {
+                  "name": "terrascan",
+                  "informationUri": "https://github.com/accurics/terrascan",
+                  "rules": [
+                    {
+                      "id": "AWS.S3Bucket.DS.High.1043",
+                      "name": "s3EnforceUserACL",
+                      "shortDescription": {
+                        "text": "S3 bucket Access is allowed to all AWS Account Users."
+                      },
+                      "properties": {
+                        "category": "S3",
+                        "severity": "HIGH"
+                      }
+                    }
+                  ]
+                }
+              },
+              "results": [
+                {
+                  "ruleId": "AWS.S3Bucket.DS.High.1043",
+                  "level": "error",
+                  "message": {
+                    "markdown": "S3 bucket Access is allowed to all AWS Account Users."
+                  },
+                  "locations": [
+                    {
+                      "physicalLocation": {
+                        "artifactLocation": {
+                          "uri": "modules/m1/main.tf"
+                        },
+                        "contextRegion": {
+                          "startLine": 20
+                        }
+                      }
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }`
+
+const expectedSarifOutput2 = `{
+          "version": "2.1.0",
+          "$schema": "https://raw.githubusercontent.com/oasis-tcs/sarif-spec/master/Schemata/sarif-schema-2.1.0.json",
+          "runs": [
+            {
+              "tool": {
+                "driver": {
+                  "name": "terrascan",
+                  "informationUri": "https://github.com/accurics/terrascan"
+                }
+              },
+              "results": []
+            }
+          ]
+        }`
+
+const expectedSarifOutput3 = `{
+          "version": "2.1.0",
+          "$schema": "https://raw.githubusercontent.com/oasis-tcs/sarif-spec/master/Schemata/sarif-schema-2.1.0.json",
+          "runs": [
+            {
+              "tool": {
+                "driver": {
+                  "name": "terrascan",
+                  "informationUri": "https://github.com/accurics/terrascan",
+                  "rules": [
+                    {
+                      "id": "AWS.S3Bucket.DS.High.1043",
+                      "name": "s3EnforceUserACL",
+                      "shortDescription": {
+                        "text": "S3 bucket Access is allowed to all AWS Account Users."
+                      },
+                      "properties": {
+                        "category": "S3",
+                        "severity": "HIGH"
+                      }
+                    }
+                  ]
+                }
+              },
+              "results": []
+            }
+          ]
+        }`
+
+func TestSarifWriter(t *testing.T) {
+
+	type funcInput interface{}
+	tests := []struct {
+		name           string
+		input          funcInput
+		expectedError  bool
+		expectedOutput string
+	}{
+		{
+			name:           "Human Readable Writer: Violations",
+			input:          violationsInput,
+			expectedOutput: expectedSarifOutput1,
+		},
+		{
+			name: "Human Readable Writer: No Violations",
+			input: policy.EngineOutput{
+				ViolationStore: &results.ViolationStore{
+					Summary: summaryWithNoViolations,
+				},
+			},
+			expectedOutput: expectedSarifOutput2,
+		},
+		{
+			name:           "Human Readable Writer: With PassedRules",
+			input:          outputWithPassedRules,
+			expectedOutput: expectedSarifOutput3,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			writer := &bytes.Buffer{}
+			if err := SarifWriter(tt.input, writer); (err != nil) != tt.expectedError {
+				t.Errorf("HumanReadbleWriter() error = gotErr: %v, wantErr: %v", err, tt.expectedError)
+			}
+			outputBytes := writer.Bytes()
+			gotOutput := string(bytes.TrimSpace(outputBytes))
+
+			if equal, _ := utils.AreEqualJSON(strings.TrimSpace(gotOutput), strings.TrimSpace(tt.expectedOutput)); !equal {
+				t.Errorf("HumanReadbleWriter() = got: %v, want: %v", gotOutput, tt.expectedOutput)
+			}
+		})
+	}
+}
