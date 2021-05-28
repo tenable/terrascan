5e4ca8c35be317851ad59d54ffba85540616e840
Pankaj Patil <patilpankaj212@gmail.com>
2021-05-28 19:14:51 +0530

    config reader pending test changes

--- pkg/config/global_test.go
+++ pkg/config/global_test.go
@@ -17,13 +17,15 @@
 package config
 
 import (
+	"path/filepath"
+	"reflect"
 	"testing"
 
 	"github.com/accurics/terrascan/pkg/utils"
 )
 
 func TestLoadGlobalConfig(t *testing.T) {
-	testConfigFile := "./testdata/terrascan-config.toml"
+	testConfigFile := filepath.Join(testDataDir, "terrascan-config-all-fields.toml")
 	absDefaultBasePolicyPath, absDefaultPolicyRepoPath, _ := utils.GetAbsPolicyConfigPaths(defaultBasePolicyPath, defaultPolicyRepoPath)
 	absCustomPath, absRegoSubdirPath, _ := utils.GetAbsPolicyConfigPaths("custom-path", "rego-subdir")
 
@@ -38,6 +40,12 @@ func TestLoadGlobalConfig(t *testing.T) {
 		policyRepoPath string
 		repoURL        string
 		branchName     string
+		scanRules      []string
+		skipRules      []string
+		severity       string
+		categories     []string
+		notifications  map[string]Notifier
+		k8sAdmControl  K8sAdmissionControl
 	}{
 		{
 			// no error expected
@@ -70,6 +78,14 @@ func TestLoadGlobalConfig(t *testing.T) {
 			policyRepoPath: absRegoSubdirPath,
 			repoURL:        "https://repository/url",
 			branchName:     "branch-name",
+			scanRules:      testRules.ScanRules,
+			skipRules:      testRules.SkipRules,
+			severity:       highSeverity.Level,
+			categories:     testCategoryList.List,
+			notifications: map[string]Notifier{
+				"webhook1": testNotifier,
+			},
+			k8sAdmControl: testK8sAdmControl,
 		},
 	}
 	for _, tt := range tests {
@@ -80,6 +96,14 @@ func TestLoadGlobalConfig(t *testing.T) {
 
 			if GetPolicyBasePath() != tt.policyBasePath || GetPolicyRepoPath() != tt.policyRepoPath || GetPolicyRepoURL() != tt.repoURL || GetPolicyBranch() != tt.branchName {
 				t.Errorf("LoadGlobalConfig() error = got BasePath: %v, RepoPath: %v, RepoURL: %v, BranchName: %v, want BasePath: %v, RepoPath: %v, RepoURL: %v, BranchName: %v", GetPolicyBasePath(), GetPolicyRepoPath(), GetPolicyRepoURL(), GetPolicyBranch(), tt.policyBasePath, tt.policyRepoPath, tt.repoURL, tt.branchName)
+			}
+
+			if !utils.IsSliceEqual(GetScanRules(), tt.scanRules) || !utils.IsSliceEqual(GetSkipRules(), tt.skipRules) || !utils.IsSliceEqual(GetCategoryList(), tt.categories) || GetSeverityLevel() != tt.severity {
+				t.Errorf("LoadGlobalConfig() error = got scan rules: %v, skip rules: %v, categories: %v, severity: %v, want scan rules: %v, skip rules: %v, categories: %v, severity: %v", GetScanRules(), GetSkipRules(), GetCategoryList(), GetSeverityLevel(), tt.scanRules, tt.skipRules, tt.categories, tt.severity)
+			}
+
+			if !reflect.DeepEqual(GetNotifications(), tt.notifications) || !reflect.DeepEqual(GetK8sAdmissionControl(), tt.k8sAdmControl) {
+				t.Errorf("LoadGlobalConfig() error = got notifications: %v, k8s admission control: %v, want notifications: %v, k8s admission control: %v", GetNotifications(), GetK8sAdmissionControl(), tt.notifications, tt.k8sAdmControl)
 			}
 		})
 	}
