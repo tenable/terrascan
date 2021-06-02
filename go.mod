eaf1e76b0bb4c5a0f8c654d46fc3787bf5ddb178
Mahendra Bagul <bagulm123@gmail.com>
2021-06-02 15:45:48 +0530

    added kai monkey tests for CFT

--- go.mod
+++ go.mod
@@ -9,7 +9,9 @@ replace (
 )
 
 require (
+	github.com/awslabs/goformation/v4 v4.19.1
 	github.com/ghodss/yaml v1.0.0
+	github.com/google/uuid v1.2.0 // indirect
 	github.com/gorilla/handlers v1.5.1
 	github.com/gorilla/mux v1.8.0
 	github.com/hashicorp/go-cleanhttp v0.5.1
@@ -22,7 +24,6 @@ require (
 	github.com/hashicorp/terraform v0.14.4
 	github.com/hashicorp/terraform-svchost v0.0.0-20200729002733-f050f53b9734
 	github.com/iancoleman/strcase v0.1.3
-	github.com/infracloudio/mapper v0.0.0-20210511004214-d7ba0fe83ea1
 	github.com/itchyny/gojq v0.12.1
 	github.com/mattn/go-isatty v0.0.12
 	github.com/mattn/go-sqlite3 v1.12.0
@@ -35,6 +36,7 @@ require (
 	github.com/pkg/errors v0.9.1
 	github.com/spf13/afero v1.5.1
 	github.com/spf13/cobra v1.1.1
+	github.com/stretchr/testify v1.7.0
 	github.com/zclconf/go-cty v1.8.2
 	go.uber.org/zap v1.16.0
 	golang.org/x/sys v0.0.0-20210525143221-35b2ab0089ea
--- pkg/iac-providers/cft/v1/load-dir_test.go
+++ pkg/iac-providers/cft/v1/load-dir_test.go
@@ -18,6 +18,10 @@ package cftv1
 
 import (
 	"fmt"
+	"go.uber.org/zap"
+	"gopkg.in/src-d/go-git.v4"
+	gitConfig "gopkg.in/src-d/go-git.v4/config"
+	"gopkg.in/src-d/go-git.v4/plumbing"
 	"os"
 	"path/filepath"
 	"syscall"
@@ -27,6 +31,20 @@ import (
 	"github.com/accurics/terrascan/pkg/utils"
 	"github.com/hashicorp/go-multierror"
 )
+
+const (
+	repoURL  = "https://github.com/accurics/KaiMonkey.git"
+	branch   = "master"
+	basePath = "artifacts"
+	provider = "cft"
+)
+
+func TestMain(m *testing.M) {
+	setup()
+	code := m.Run()
+	shutdown()
+	os.Exit(code)
+}
 
 func TestLoadIacDir(t *testing.T) {
 	testDataDir := "testdata"
@@ -85,4 +103,74 @@ func TestLoadIacDir(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestCFTMapper(t *testing.T) {
+	root := filepath.Join(basePath, provider)
+	dirList := make([]string, 0)
+	err := filepath.Walk(root, func(filePath string, fileInfo os.FileInfo, err error) error {
+		if fileInfo != nil && fileInfo.IsDir() {
+			dirList = append(dirList, filePath)
+		}
+		return err
+	})
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	cftv1 := CFTV1{}
+	for i := 1; i < len(dirList); i++ {
+		dir := dirList[i]
+		t.Run(dir, func(t *testing.T) {
+			_, gotErr := cftv1.LoadIacDir(dir, false)
+			_, ok := gotErr.(*multierror.Error)
+			if !ok {
+				t.Errorf("expected multierror.Error, got %T", gotErr)
+			}
+		})
+	}
+}
+
+func setup() {
+	err := downloadArtifacts()
+	if err != nil {
+		zap.S().Fatal(err)
+	}
+}
+
+func shutdown() {
+	_ = os.RemoveAll(basePath)
+}
+
+func downloadArtifacts() error {
+	os.RemoveAll(basePath)
+
+	r, err := git.PlainClone(basePath, false, &git.CloneOptions{
+		URL: repoURL,
+	})
+	if err != nil {
+		return err
+	}
+
+	w, err := r.Worktree()
+	if err != nil {
+		return err
+	}
+
+	err = r.Fetch(&git.FetchOptions{
+		RefSpecs: []gitConfig.RefSpec{"refs/*:refs/*", "HEAD:refs/heads/HEAD"},
+	})
+	if err != nil {
+		return err
+	}
+
+	err = w.Checkout(&git.CheckoutOptions{
+		Branch: plumbing.ReferenceName(fmt.Sprintf("refs/heads/%s", branch)),
+		Force:  true,
+	})
+	if err != nil {
+		return err
+	}
+	return nil
 }
