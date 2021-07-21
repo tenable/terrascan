4e72b4d3083671338f2c272b84ffe58d47e5900b
Devang <devang.gaur.7@gmail.com>
2021-07-21 20:37:41 +0530

    increase kubernetes resource scan coverage

--- pkg/iac-providers/terraform/commons/extract-container-images.go
+++ pkg/iac-providers/terraform/commons/extract-container-images.go
@@ -21,6 +21,7 @@ var eligibiltylist []ResourceMetadata = []ResourceMetadata{
 	{ProviderType: "kubernetes", ResourceType: "kubernetes_job"},
 	{ProviderType: "kubernetes", ResourceType: "kubernetes_cron_job"},
 	{ProviderType: "kubernetes", ResourceType: "kubernetes_daemonset"},
+	{ProviderType: "kubernetes", ResourceType: "kubernetes_replication_controller"},
 }
 
 
@@ -46,11 +47,16 @@ func isEligibleForContainerImageExtraction(resource *hclConfigs.Resource, reqdPr
 }
 
 func extractContainerImages(resource *hclConfigs.Resource, body *hclsyntax.Body) []error {
-	zap.S().Info("Dump Round 1")
+//  zap.S().Info("Dumping resource body")
 //	spew.Dump(body)
 
 	if strings.ToLower(resource.Provider.Type) == "kubernetes" {
-		if strings.ToLower(resource.Type) == "kubernetes_deployment" {
+		if strings.ToLower(resource.Type) == "kubernetes_deployment" ||
+			strings.ToLower(resource.Type) == "kubernetes_daemonset" ||
+			strings.ToLower(resource.Type) == "kubernetes_job" ||
+			strings.ToLower(resource.Type) == "kubernetes_replication_controller" ||
+			strings.ToLower(resource.Type) == "kubernetes_stateful_set" {
+			zap.S().Infof("Inside resource type %s", resource.Type)
 			for _, block := range body.Blocks {
 				if block.Type == "spec" {
 					zap.S().Info("Inside spec")
@@ -69,16 +75,118 @@ func extractContainerImages(resource *hclConfigs.Resource, body *hclsyntax.Body)
 												if attr.Name == "image" {
 													val, _ := attr.Expr.Value(nil)
 													//spew.Dump(val)
-													zap.S().Info(val.AsString())
+													zap.S().Info("image: %s", val.AsString())
 												}
 												if attr.Name == "name" {
 													val, _ := attr.Expr.Value(nil)
 													//spew.Dump(val)
-													zap.S().Info(val.AsString())
+													zap.S().Infof("name: %s", val.AsString())
+												}
+											}
+										}
+										if block3.Type == "init_container" {
+											zap.S().Info("Inside spec.template.spec.init_container")
+											//spew.Dump(block3)
+											for _, attr := range block3.Body.Attributes {
+												if attr.Name == "image" {
+													val, _ := attr.Expr.Value(nil)
+													//spew.Dump(val)
+													zap.S().Infof("image: %s", val.AsString())
+												}
+												if attr.Name == "name" {
+													val, _ := attr.Expr.Value(nil)
+													//spew.Dump(val)
+													zap.S().Infof("name: %s", val.AsString())
 												}
 											}
 										}
 									}
+								}
+							}
+						}
+					}
+				}
+			}
+		} else if strings.ToLower(resource.Type) == "kubernetes_cron_job" {
+			zap.S().Infof("Inside resource type %s", resource.Type)
+			for _, block := range body.Blocks {
+				if block.Type == "spec" {
+					zap.S().Info("Inside spec")
+					for _, block1 := range block.Body.Blocks {
+						if block1.Type == "job_template" {
+							zap.S().Info("Inside spec.job_template")
+							for _, block2 := range block1.Body.Blocks {
+								if block2.Type == "spec" {
+									zap.S().Info("Inside spec.job_template.spec")
+									for _, block3 := range block2.Body.Blocks {
+										if block3.Type == "template" {
+											zap.S().Info("Inside spec.job_template.spec.template")
+											for _, block4 := range block3.Body.Blocks {
+												zap.S().Infof("block type : %s", block2.Type)
+												if block4.Type == "spec" {
+													zap.S().Info("Inside spec.job_template.spec.template.spec")
+													for _, block5 := range block4.Body.Blocks {
+														if block5.Type == "container" {
+															zap.S().Info("Inside spec.job_template.spec.template.spec.container")
+															//spew.Dump(block3)
+															for _, attr := range block5.Body.Attributes {
+																if attr.Name == "image" {
+																	val, _ := attr.Expr.Value(nil)
+																	//spew.Dump(val)
+																	zap.S().Info("image: %s", val.AsString())
+																}
+																if attr.Name == "name" {
+																	val, _ := attr.Expr.Value(nil)
+																	//spew.Dump(val)
+																	zap.S().Infof("name: %s", val.AsString())
+																}
+															}
+														}
+														if block5.Type == "init_container" {
+															zap.S().Info("Inside spec.job_template.spec.template.spec.init_container")
+															//spew.Dump(block3)
+															for _, attr := range block5.Body.Attributes {
+																if attr.Name == "image" {
+																	val, _ := attr.Expr.Value(nil)
+																	//spew.Dump(val)
+																	zap.S().Infof("image: %s", val.AsString())
+																}
+																if attr.Name == "name" {
+																	val, _ := attr.Expr.Value(nil)
+																	//spew.Dump(val)
+																	zap.S().Infof("name: %s", val.AsString())
+																}
+															}
+														}
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		} else if strings.ToLower(resource.Type) == "kubernetes_pod" {
+			for _, block := range body.Blocks {
+				if block.Type == "spec" {
+					zap.S().Info("Inside spec")
+					for _, block1 := range block.Body.Blocks {
+						if block1.Type == "container" {
+							zap.S().Info("Inside spec.container")
+							//spew.Dump(block3)
+							for _, attr := range block1.Body.Attributes {
+								if attr.Name == "image" {
+									val, _ := attr.Expr.Value(nil)
+									//spew.Dump(val)
+									zap.S().Info(val.AsString())
+								}
+								if attr.Name == "name" {
+									val, _ := attr.Expr.Value(nil)
+									//spew.Dump(val)
+									zap.S().Info(val.AsString())
 								}
 							}
 						}
---
+++ pkg/iac-providers/terraform/v12/testdata/terraform-k8s-provider/stateful_set.tf
@@ -0,0 +1,183 @@
+resource "kubernetes_stateful_set" "prometheus" {
+  metadata {
+    annotations = {
+      SomeAnnotation = "foobar"
+    }
+
+    labels = {
+      k8s-app                           = "prometheus"
+      "kubernetes.io/cluster-service"   = "true"
+      "addonmanager.kubernetes.io/mode" = "Reconcile"
+      version                           = "v2.2.1"
+    }
+
+    name = "prometheus"
+  }
+
+  spec {
+    pod_management_policy  = "Parallel"
+    replicas               = 1
+    revision_history_limit = 5
+
+    selector {
+      match_labels = {
+        k8s-app = "prometheus"
+      }
+    }
+
+    service_name = "prometheus"
+
+    template {
+      metadata {
+        labels = {
+          k8s-app = "prometheus"
+        }
+
+        annotations = {}
+      }
+
+      spec {
+        service_account_name = "prometheus"
+
+        init_container {
+          name              = "init-chown-data"
+          image             = "busybox:latest"
+          image_pull_policy = "IfNotPresent"
+          command           = ["chown", "-R", "65534:65534", "/data"]
+
+          volume_mount {
+            name       = "prometheus-data"
+            mount_path = "/data"
+            sub_path   = ""
+          }
+        }
+
+        container {
+          name              = "prometheus-server-configmap-reload"
+          image             = "jimmidyson/configmap-reload:v0.1"
+          image_pull_policy = "IfNotPresent"
+
+          args = [
+            "--volume-dir=/etc/config",
+            "--webhook-url=http://localhost:9090/-/reload",
+          ]
+
+          volume_mount {
+            name       = "config-volume"
+            mount_path = "/etc/config"
+            read_only  = true
+          }
+
+          resources {
+            limits = {
+              cpu    = "10m"
+              memory = "10Mi"
+            }
+
+            requests = {
+              cpu    = "10m"
+              memory = "10Mi"
+            }
+          }
+        }
+
+        container {
+          name              = "prometheus-server"
+          image             = "prom/prometheus:v2.2.1"
+          image_pull_policy = "IfNotPresent"
+
+          args = [
+            "--config.file=/etc/config/prometheus.yml",
+            "--storage.tsdb.path=/data",
+            "--web.console.libraries=/etc/prometheus/console_libraries",
+            "--web.console.templates=/etc/prometheus/consoles",
+            "--web.enable-lifecycle",
+          ]
+
+          port {
+            container_port = 9090
+          }
+
+          resources {
+            limits = {
+              cpu    = "200m"
+              memory = "1000Mi"
+            }
+
+            requests = {
+              cpu    = "200m"
+              memory = "1000Mi"
+            }
+          }
+
+          volume_mount {
+            name       = "config-volume"
+            mount_path = "/etc/config"
+          }
+
+          volume_mount {
+            name       = "prometheus-data"
+            mount_path = "/data"
+            sub_path   = ""
+          }
+
+          readiness_probe {
+            http_get {
+              path = "/-/ready"
+              port = 9090
+            }
+
+            initial_delay_seconds = 30
+            timeout_seconds       = 30
+          }
+
+          liveness_probe {
+            http_get {
+              path   = "/-/healthy"
+              port   = 9090
+              scheme = "HTTPS"
+            }
+
+            initial_delay_seconds = 30
+            timeout_seconds       = 30
+          }
+        }
+
+        termination_grace_period_seconds = 300
+
+        volume {
+          name = "config-volume"
+
+          config_map {
+            name = "prometheus-config"
+          }
+        }
+      }
+    }
+
+    update_strategy {
+      type = "RollingUpdate"
+
+      rolling_update {
+        partition = 1
+      }
+    }
+
+    volume_claim_template {
+      metadata {
+        name = "prometheus-data"
+      }
+
+      spec {
+        access_modes       = ["ReadWriteOnce"]
+        storage_class_name = "standard"
+
+        resources {
+          requests = {
+            storage = "16Gi"
+          }
+        }
+      }
+    }
+  }
+}
