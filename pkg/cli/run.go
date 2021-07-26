dd61854025d46fa5df447d7d32cd89ee589cf9b5
Pankaj Patil <patilpankaj212@gmail.com>
2021-07-26 18:37:06 +0530

    fix - config only option used with remote repo scan generates panic (#948)

--- pkg/cli/run.go
+++ pkg/cli/run.go
@@ -196,7 +196,7 @@ func (s *ScanOptions) Run() error {
 	}
 
 	// set the ResourcePath to remoteURL if remote directory is scanned.
-	if s.remoteURL != "" {
+	if !s.configOnly && s.remoteURL != "" {
 		results.Violations.ViolationStore.Summary.ResourcePath = s.remoteURL
 	}
 
