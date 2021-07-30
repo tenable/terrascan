c0db34d401a153c6eef5776acb27c53cb3df45dd
mihirhasan <cs-mihirhasan@accurics.com>
2021-07-29 23:55:01 -0700

    Update pre-commit-hook.sh

--- scripts/pre-commit-hook.sh
+++ scripts/pre-commit-hook.sh
@@ -28,7 +28,7 @@ terrascan_() {
   for path_uniq in $(echo "${paths[*]}" | tr ' ' '\n' | sort -u); do
     path_uniq="${path_uniq//__REPLACED__SPACE__/ }"
     pushd "$path_uniq" > /dev/null
-    terrascan scan $ARGS
+    terrascan scan PARAMS
     popd > /dev/null
   done
 }
