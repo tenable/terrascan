b6c73d744b1f915f4700414a3050745fc3c3925a
mihirhasan <cs-mihirhasan@accurics.com>
2021-07-29 23:41:25 -0700

    Update pre-commit-hook.sh

--- scripts/pre-commit-hook.sh
+++ scripts/pre-commit-hook.sh
@@ -19,7 +19,7 @@ terrascan_() {
     let "index+=1"
   done
   #put arguments array into runnable string 
-  PARAMS = ""
+  local PARAMS = " "
   for i in "${arr[@]}"
   do
    PARAMS= "${PARAMS} ${i}"
