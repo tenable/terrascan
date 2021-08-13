88d9a5a5ec69fb35acb1595ec7fa97be2a4e48e9
mihirhasan <cs-mihirhasan@accurics.com>
2021-08-13 15:02:34 -0700

    Update docker-build.sh

--- scripts/atlantis/docker-build.sh
+++ scripts/atlantis/docker-build.sh
@@ -6,6 +6,6 @@ set -o pipefail
 
 GIT_COMMIT=$(git rev-parse --short HEAD 2>/dev/null)
 DOCKER_REPO="accurics/terrascan_atlantis"
-DIR="../.."
+DIR="./integrations/atlantis"
 
 docker build -t ${DOCKER_REPO}:${GIT_COMMIT} ${DIR}
