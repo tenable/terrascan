d268c20f88ba042894f68738c3f1f960866ce4da
mihirhasan <cs-mihirhasan@accurics.com>
2021-08-18 15:26:14 -0700

    Update run.go

--- pkg/initialize/run.go
+++ pkg/initialize/run.go
@@ -103,7 +103,7 @@ func DownloadPolicies() error {
 	// checkout policies branch
 	release,err := config.GetLatestTag(r)
 	err = w.Checkout(&git.CheckoutOptions{
-		Branch: plumbing.ReferenceName(fmt.Sprintf("refs/tags/%s", release)),
+		Branch: plumbing.ReferenceName("refs/tags/" + release),
 		//Branch: plumbing.ReferenceName(fmt.Sprintf("refs/heads/%s", branch)),
 		//Hash: plumbing.NewHash("39401c4ca2d8377b100050ecccb037551a0b785c"),
 		Force:  true,
