85826c35f661b4240f905170aa3681286215a671
mihirhasan <cs-mihirhasan@accurics.com>
2021-08-19 11:10:17 -0700

    Update scan.go

--- pkg/cli/scan.go
+++ pkg/cli/scan.go
@@ -39,6 +39,9 @@ Detect compliance and security violations across Infrastructure as Code to mitig
 		if scanOptions.configOnly {
 			return nil
 		}
+		fmt.Println("----------")
+		fmt.Println(scanOptions.policyPath)
+		fmt.Println("----------")
 		return initial(cmd, args, true)
 	},
 	RunE:          scan,
