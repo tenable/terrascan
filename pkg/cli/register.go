b0259e8b0650a2a4b9a6a6f3b8ca31745cfa9515
Suvarna Rokade <suvarnarokade11@gmail.com>
2022-01-11 20:24:42 +0530

    Adds: logger in case of flag parsing fails (#1115)
    * adds: logger in case of flag parsing fails
    
    * fix: error check
    
    Co-authored-by: Suvarna Rokade <suvarna.rokade@accurics.com>

--- pkg/cli/register.go
+++ pkg/cli/register.go
@@ -21,6 +21,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/accurics/terrascan/pkg/config"
 	"github.com/accurics/terrascan/pkg/logging"
@@ -39,6 +40,9 @@ func Execute() {
 	rootCmd.PersistentFlags().StringVarP(&LogType, "log-type", "x", "console", "log output type (console, json)")
 	rootCmd.PersistentFlags().StringVarP(&OutputType, "output", "o", "human", "output type (human, json, yaml, xml, junit-xml, sarif, github-sarif)")
 	rootCmd.PersistentFlags().StringVarP(&ConfigFile, "config-path", "c", "", "config file path")
+
+	//Added init here in case flag parsing failed we should log which flag was incorrect.
+	logging.Init(LogType, LogLevel)
 
 	// Function to execute before processing commands
 	cobra.OnInitialize(func() {
@@ -77,6 +81,11 @@ func Execute() {
 	}
 
 	if err := rootCmd.Execute(); err != nil {
+		// check if the error is related to flag argument missing and
+		// log it before terminating the process so user gets idea about the incorrect flag value
+		if strings.Contains(err.Error(), "flag needs an argument") {
+			zap.S().Error("error while executing command ", zap.Error(err))
+		}
 		os.Exit(1)
 	}
 }
