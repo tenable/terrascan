90db4adf0359d77290483d0988cfdff1e727abad
Gaurav Gogia <16029099+gaurav-gogia@users.noreply.github.com>
2022-03-09 01:43:33 +0530

    test cases updated

--- pkg/iac-providers/cft/v1/load-file_test.go
+++ pkg/iac-providers/cft/v1/load-file_test.go
@@ -32,10 +32,37 @@ func TestLoadIacFile(t *testing.T) {
 	invalidFile, _ := filepath.Abs(path.Join(testDataDir, "deploy.yaml"))
 	validFile, _ := filepath.Abs(path.Join(testDataDir, "templates", "s3", "deploy.template"))
 	nestedFile, _ := filepath.Abs(path.Join(testDataDir, "templates", "s3", "nested.template"))
+	partialWrongFile, _ := filepath.Abs(path.Join(testDataDir, "someResourcesIncorrectCftTemplate.yml"))
 
 	testErrString1 := fmt.Sprintf("unsupported extension for file %s", testFile)
 	testErrString2 := "unable to read file nonexistent.txt"
 	testErrString3 := "invalid YAML template: yaml: line 27: did not find expected alphabetic or numeric character"
+
+	validFileConfig := make(map[string][]output.ResourceConfig, 2)
+	validFileConfig["aws_s3_bucket_policy"] = []output.ResourceConfig{{
+		ID: "aws_s3_bucket_policy.BucketPolicy",
+	}}
+	validFileConfig["aws_s3_bucket"] = []output.ResourceConfig{{
+		ID: "aws_s3_bucket.S3Bucket",
+	}}
+
+	nestedFileConfig := make(map[string][]output.ResourceConfig, 2)
+	nestedFileConfig["aws_cloudformation_stack"] = []output.ResourceConfig{{
+		ID: "aws_cloudformation_stack.myStackWithParams",
+	}}
+	nestedFileConfig["aws_s3_bucket"] = []output.ResourceConfig{{
+		ID: "aws_s3_bucket.S3Bucket",
+	}}
+
+	partialWrongConfig := make(map[string][]output.ResourceConfig, 3)
+	partialWrongConfig["aws_cognito_user_pool"] = []output.ResourceConfig{{
+		ID: "aws_cognito_user_pool.goodpool",
+	}}
+	partialWrongConfig["aws_kinesis_stream"] = []output.ResourceConfig{{
+		ID: "aws_kinesis_stream.riverstream",
+	}, {
+		ID: "aws_kinesis_stream.livestream",
+	}}
 
 	table := []struct {
 		wantErr  error
@@ -69,29 +96,77 @@ func TestLoadIacFile(t *testing.T) {
 			typeOnly: false,
 		}, {
 			wantErr:  nil,
-			want:     output.AllResourceConfigs{},
+			want:     validFileConfig,
 			cftv1:    CFTV1{},
-			name:     "invalid file",
+			name:     "valid file",
 			filePath: validFile,
 			typeOnly: false,
 		}, {
 			wantErr:  nil,
-			want:     output.AllResourceConfigs{},
+			want:     nestedFileConfig,
 			cftv1:    CFTV1{},
 			name:     "nested file",
 			filePath: nestedFile,
+			typeOnly: false,
+		},
+		{
+			wantErr:  nil,
+			want:     partialWrongConfig,
+			cftv1:    CFTV1{},
+			name:     "partially wrong cft file",
+			filePath: partialWrongFile,
 			typeOnly: false,
 		},
 	}
 
 	for _, tt := range table {
 		t.Run(tt.name, func(t *testing.T) {
-			_, gotErr := tt.cftv1.LoadIacFile(tt.filePath, tt.options)
+			gotResponse, gotErr := tt.cftv1.LoadIacFile(tt.filePath, tt.options)
+
 			if !reflect.DeepEqual(gotErr, tt.wantErr) {
 				t.Errorf("unexpected error; gotErr: '%+v', wantErr: '%+v'", gotErr, tt.wantErr)
-			} else if tt.typeOnly && (reflect.TypeOf(gotErr)) != reflect.TypeOf(tt.wantErr) {
+			}
+
+			if tt.typeOnly && (reflect.TypeOf(gotErr)) != reflect.TypeOf(tt.wantErr) {
 				t.Errorf("unexpected error; gotErr: '%v', wantErr: '%v'", reflect.TypeOf(gotErr), reflect.TypeOf(tt.wantErr))
+			}
+
+			if err := verifyResponseObject(tt.want, gotResponse); err != nil {
+				t.Error(err)
 			}
 		})
 	}
+}
+
+func verifyResponseObject(want, got output.AllResourceConfigs) error {
+	if len(got) == 0 && len(want) == 0 {
+		return nil
+	}
+
+	if len(want) != len(got) {
+		return fmt.Errorf("incorrect response object array length; got: '%d', want: '%d'", len(got), len(want))
+	}
+
+	for wantKey := range want {
+
+		if _, ok := got[wantKey]; !ok {
+			return fmt.Errorf("wanted resource object not found in response object")
+		}
+
+		for wantIndex := range want[wantKey] {
+
+			flag := false
+			for gotIndex := range got[wantKey] {
+				if got[wantKey][gotIndex].ID == want[wantKey][gotIndex].ID {
+					flag = true
+				}
+			}
+			if !flag {
+				return fmt.Errorf("resource ID mismatch for key: '%s' at index: '%d'", wantKey, wantIndex)
+			}
+
+		}
+	}
+
+	return nil
 }
