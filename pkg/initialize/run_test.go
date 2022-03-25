bef69fb18eda354058ff4d63a4bfdf909809280c
Gaurav Gogia <16029099+gaurav-gogia@users.noreply.github.com>
2022-03-25 05:37:22 +0530

    add test case for conversion

--- pkg/initialize/run_test.go
+++ pkg/initialize/run_test.go
@@ -16,10 +16,135 @@
 
 package initialize
 
-import "testing"
+import (
+	"bytes"
+	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+)
 
-func TestConvertCommercialPolicies(t *testing.T) {
-	t.Run("multi folder policies", testRunConvertCommercialPolicies)
+const TESTDIR = "./test_data/"
+
+func TestGetCommercialPolicy(t *testing.T) {
+	policies, err := ioutil.ReadFile("./test_data/policies.json")
+	if err != nil {
+		t.Errorf("unable to read test file")
+	}
+
+	tempDir, err := os.MkdirTemp("", "test_policies")
+	if err != nil {
+		t.Errorf("unable to create temporary dir for testing")
+	}
+	defer os.RemoveAll(tempDir)
+
+	expectedCspMap := map[string]string{
+		"aws":   "aws_cloudwatch_log_group",
+		"azure": "azurerm_security_center_setting",
+		"gcp":   "google_compute_firewall",
+	}
+
+	err = convertCommercialPolicies(policies, tempDir)
+	if err != nil {
+		t.Errorf("unable to convert and save policies: '%v'", err)
+	}
+
+	csps, err := os.ReadDir(tempDir)
+	if err != nil {
+		t.Errorf("unable to read temp dir: '%v'", err)
+	}
+	if len(csps) != 3 {
+		t.Errorf("expected length 3; got: '%d'", len(csps))
+	}
+
+	for _, csp := range csps {
+		cspdir := filepath.Join(tempDir, csp.Name())
+		rscs, err := os.ReadDir(cspdir)
+		if err != nil {
+			t.Errorf("unable to read csp dir: '%v'", err)
+			break
+		}
+		if len(rscs) != 1 {
+			t.Errorf("expected length 1; got '%d'", len(rscs))
+			break
+		}
+
+		val, ok := expectedCspMap[csp.Name()]
+		if !ok {
+			t.Errorf("unable to find expected csp")
+			break
+		}
+		if rscs[0].Name() != val {
+			t.Errorf("unable to find expected resource type")
+			break
+		}
+
+		rscdir := filepath.Join(cspdir, rscs[0].Name())
+		files, err := os.ReadDir(rscdir)
+		if err != nil {
+			t.Errorf("unable to read resource type dir: '%v'", err)
+		}
+
+		err = verifyFiles(files, rscdir)
+		if err != nil {
+			t.Error(err)
+			break
+		}
+	}
 }
 
-func testRunConvertCommercialPolicies(t *testing.T) {}
+func verifyFiles(files []fs.DirEntry, rscdir string) error {
+	expectedMetaMap := map[string]bool{
+		"AC_AWS_0452.json":   true,
+		"AC_AZURE_0330.json": true,
+		"AC_GCP_0095.json":   true,
+	}
+
+	expectedRegoMap := map[string]bool{
+		"awsNoRetentionPolicyCloudwatchLogGroup.rego":   true,
+		"ensureProperSettings_25082021.rego":            true,
+		"networkPortExposedToInternetGCP_25082021.rego": true,
+	}
+
+	if len(files) != 2 {
+		return fmt.Errorf("expected length 2; got: '%d'", len(files))
+	}
+
+	for _, file := range files {
+		if strings.HasSuffix(file.Name(), ".json") {
+			_, ok := expectedMetaMap[file.Name()]
+			if !ok {
+				return fmt.Errorf("expected metadata file not found")
+			}
+		}
+
+		if strings.HasSuffix(file.Name(), ".rego") {
+			_, ok := expectedRegoMap[file.Name()]
+			if !ok {
+				return fmt.Errorf("expected rego file not found")
+			}
+		}
+
+		testpath := filepath.Join(TESTDIR, file.Name())
+		convpath := filepath.Join(rscdir, file.Name())
+
+		testbytes, err := ioutil.ReadFile(testpath)
+		if err != nil {
+			return fmt.Errorf("unable to read test file: '%s', err: '%v'", testpath, err)
+		}
+
+		databytes, err := ioutil.ReadFile(convpath)
+		if err != nil {
+			return fmt.Errorf("unable to read converted file: '%s', err: '%v'", convpath, err)
+		}
+
+		if bytes.Compare(testbytes, databytes) != 0 {
+			return fmt.Errorf("test file and converted file do not match")
+		}
+	}
+
+	return nil
+}
