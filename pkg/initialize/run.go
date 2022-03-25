8c14122c05c47af8c0593f95f2dd4e36b9562ef5
Gaurav Gogia <16029099+gaurav-gogia@users.noreply.github.com>
2022-03-25 11:44:31 +0530

    add response status code

--- pkg/initialize/run.go
+++ pkg/initialize/run.go
@@ -107,7 +107,7 @@ func downloadCommercialPolicies(policyBasePath, accessToken string) error {
 	req.Header.Add("Authorization", "Bearer "+accessToken)
 	res, err := client.Do(req)
 	if err != nil || res.StatusCode != 200 {
-		return fmt.Errorf("error downloading commercial policies. error: '%v', response code: '%d'", err, res.StatusCode)
+		return fmt.Errorf("error downloading commercial policies. error: '%v', response status code: '%d'", err, res.StatusCode)
 	}
 	if res == nil {
 		return fmt.Errorf("error could not download policies, please check your network connection")
