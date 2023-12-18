de1b1d9164124805e2bb90255750446bd0e994c8
Nitin More <nmore@tenable.com>
2023-12-18 16:27:48 +0530

    increment version

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -19,7 +19,7 @@ package version
 import "fmt"
 
 // Terrascan The Terrascan version
-const Terrascan = "1.18.8"
+const Terrascan = "1.18.9"
 
 // Get returns the terrascan version
 func Get() string {
