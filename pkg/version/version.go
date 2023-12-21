42d528c2a7118769dcb9c7c7301b6c08de3ca589
Nitin More <nmore@tenable.com>
2023-12-21 19:15:03 +0530

    Fix e2e test due to double digit minor version

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -19,7 +19,7 @@ package version
 import "fmt"
 
 // Terrascan The Terrascan version
-const Terrascan = "1.18.10"
+const Terrascan = "1.18.11"
 
 // Get returns the terrascan version
 func Get() string {
